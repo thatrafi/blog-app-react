5fa1cd54f5b8b2f8bd3218b6b4cefea019a19d2d
George Mylonas <mylonasg88@gmail.com>
2022-03-22 15:36:56 +0800

    Created Categories component.

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -1,36 +1,39 @@
-import { authAction } from "../reducers/authReducer";
+import { authAction } from '../reducers/authReducer';
 
-export const getAuthentication = (userData) =>{
-    return async (dispatch) => {
-        // you can dispatch reducer here
-        const sendRequest = async () =>{
-            const response = await fetch('https://blog-app-react-fb813-default-rtdb.asia-southeast1.firebasedatabase.app/admin.json',
-            {
-                method : 'GET',
-            }
-            );
-            if(!response.ok){
-                throw new Error('Getting admin data failed!')
-            }
+export const getAuthentication = (userData) => {
+	return async (dispatch) => {
+		// you can dispatch reducer here
+		const sendRequest = async () => {
+			const response = await fetch(
+				'https://blog-app-react-fb813-default-rtdb.asia-southeast1.firebasedatabase.app/admin.json',
+				{
+					method: 'GET',
+				}
+			);
 
-            const data = response.json()
-            return data
-        }
+			if (!response.ok) {
+				throw new Error('Getting admin data failed!');
+			}
 
-        try {
-           const responseData =  await sendRequest();
-           const adminData = responseData.find( u => u.username === userData.username)
-           if(adminData){
-               if(adminData.password === userData.password){
-                   dispatch(authAction.authenticate({message:'success', isAuthenticated : true}))
-               }else{
-                dispatch(authAction.authenticate({message:'Wrong Password!', isAuthenticated : false}))
-               }
-           }else{
-            dispatch(authAction.authenticate({message:'No User Found', isAuthenticated : false}))
-           }
-        } catch (error) {
-            console.log(error.message);
-        }
-    }
-}
+			const data = response.json();
+			return data;
+		};
+
+		try {
+			const responseData = await sendRequest();
+			const adminData = responseData.find((u) => u.username === userData.username);
+
+			if (adminData) {
+				if (adminData.password === userData.password) {
+					dispatch(authAction.authenticate({ message: 'success', isAuthenticated: true }));
+				} else {
+					dispatch(authAction.authenticate({ message: 'Wrong Password!', isAuthenticated: false }));
+				}
+			} else {
+				dispatch(authAction.authenticate({ message: 'No User Found', isAuthenticated: false }));
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+};
--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,17 +1,19 @@
-import React from "react";
-import PostsPreview from "../components/Posts/PostsPreview";
-import Card from "../components/UI/Card";
+import React from 'react';
+import PostsPreview from '../components/Posts/PostsPreview';
+import Card from '../components/UI/Card';
+import Categories from '../containers/Categories';
 
-class Homepage extends React.Component{
-    render(){
-        return (
-            <div className="container">
-            <Card className="p-5">
-                <PostsPreview/>
-            </Card>
-        </div>
-        )
-    }
+class Homepage extends React.Component {
+	render() {
+		return (
+			<div className="container">
+				<Card className="p-5">
+					<PostsPreview />
+					<Categories />
+				</Card>
+			</div>
+		);
+	}
 }
 
 export default Homepage;
